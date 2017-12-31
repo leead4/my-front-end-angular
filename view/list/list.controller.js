ca69bd50e20f660126ff88b670fd094fcffbfa47
leead4 <leead4@gmail.com>
2017-12-31 15:06:49 -0600

    new method non destructive and reversible

--- view/list/list.controller.js
+++ view/list/list.controller.js
@@ -15,11 +15,23 @@
       { title: "To Kill a Mockingbird", author: "Harper Lee" }
     ];
 
-    $scope.removeBook = function(index) {
-      $scope.books[index].title = false;
-    };
+        $scope.hiddenBooks = [];
+        
+        $scope.hideShowBook = function(book) {
+          console.log($scope.hiddenBooks);
+            // first check if book is in hidden books array
+            if ($scope.hiddenBooks.includes(book)){
+            // if in array remove it
+                let target = $scope.hiddenBooks.indexOf(book);
+                $scope.hiddenBooks.splice(target, 1); 
+            }else{
+              $scope.hiddenBooks.push(book);
 
-  }
+            }
+            return $scope.hiddenBooks
+        }
+
+}
 
 
 }());
